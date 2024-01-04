4bce45f01fb33fa9b86949508f78bcafe80f4a5c
ks-77 <deka0987654@gmail.com>
2024-01-04 18:56:04 +0200

    completed first-task with additional variations

--- main.py
+++ main.py
@@ -18,6 +18,30 @@ if operation == "max":
 elif operation == "min":
     print(min(number_1, number_2, number_3))
 elif operation == "mean":
-    print(mean(num_list))
+    print(mean(num_list))     # print((number_1 + number_2 + number_3)/3) # print(sum(num_list)/len(num_list))
 else:
     print("Error")
+
+"""    
+if operation == "max":
+if number_1 > number_2 and number_1 > number_3:
+        print(number_1)
+    elif number_2 > number_1 and number_2 > number_3:
+        print(number_2)
+    elif number_3 > number_1 and number_3 > number_2:
+        print(number_3)
+    elif number_1 == number_2 and number_2 > number_3:
+        print(number_2)
+    elif number_1 == number_2 and number_2 < number_3:
+        print(number_3)
+    elif number_3 == number_2 and number_3 > number_1:
+        print(number_3)
+    elif number_3 == number_2 and number_3 < number_1:
+        print(number_1)
+    elif number_1 == number_3 and number_3 > number_2:
+        print(number_1)
+    elif number_1 == number_3 and number_3 < number_2:
+        print(number_2)
+    else:
+        print(number_1)     # operation if number_1 == number_2 == number_3
+"""
